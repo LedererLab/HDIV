63a2705719447f0fbae39c31f12e35e4fa16a8a2
davidagold <dag89@uw.edu>
2017-05-23 23:27:31 -0700

    work on analysis

--- src/analyze.R
+++ src/analyze.R
@@ -3,29 +3,44 @@
 
 #########################################################################
 # Dependencies
+#args <- commandArgs(trailingOnly = TRUE
 suppressMessages(library(purrr))
 suppressMessages(library(tidyr))
 suppressMessages(library(dplyr))
-suppressMessages(source("kernel/utils.R"))
+#suppressMessages(source("kernel/utils.R"))
 
 #########################################################################
 # Analysis
 
 analyze <- function() {
   args <- commandArgs(trailingOnly = TRUE)
-  df_est <- args[1] %>% read.csv() %>%
-    dplyr::select(config_id = 2, everything())
+  res_dir <- args[1]
+  project_dir <- args[2]
+  suppressMessages(source(paste(project_dir, "src/kernel/utils.R", sep = "/")))
   
-  cvg <- .cvg(df_est)
+  df_est <- paste(res_dir, "est.csv", sep = "/") %>% read.csv
+  df_stats <- paste(res_dir, "stats.csv", sep = "/") %>% read.csv
+  configs <- paste(project_dir, "src/config/configs.csv", sep = "/") %>% read.csv
+
+  coverage <- .coverage(df_est, df_stats, configs)
   print(cvg)
   
   l2bias <- .l2bias(df_est)
   print(l2bias)
 }
 
-.cvg <- function(df_est) {
+..n <- function(configs, .config_id) { filter(configs, config_id == .config_id)$n %>% as.numeric }
+..sigma_h <- function(configs, .config_id) { filter(configs, config_id == .config_id)$sigma_h %>% as.numeric }
+..sigma_hhat <- function(df_stats, .trial_id, .config_id) {
+  filter(df_stats, trial_id == .trial_id, config_id == .config_id,
+         estimator == "Debiased")$sigma_hhat %>%
+    as.numeric
+} 
+
+.coverage <- function(df_est, df_stats, configs) {
   df_est %>%
     filter(estimator == "Debiased") %>%
+    mutate(SE = vhat * ..sigma_hhat(df_stats, trial_id, config_id) / sqrt(..n(configs, config_id))) %>%
     group_by(config_id, j) %>%
     summarize(coverage_j = mean(covered(estimate_j, beta0_j, SE))) %>%
     group_by(config_id) %>%
