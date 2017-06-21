ac1c5d7c27dddac60f191f213411dbda3c400cec
David Gold <david.gold.a@gmail.com>
2017-06-21 15:04:17 -0700

    Configurations

--- src/config/make_configs.R
+++ src/config/make_configs.R
@@ -8,9 +8,9 @@ make_configs <- function() {
     # n = c(100, 400, 400, 800, 800),
     # px = c(200, 300, 500, 600, 1000),
     # pz = c(500, 600, 700, 1200, 1400)
-    n = c(100, 400, 800),
-    px = c(200, 500, 1000),
-    pz = c(500, 700, 1400)
+    n = c(100, 400, 1000),
+    px = c(250, 800, 1500),
+    pz = c(500, 1000, 1750)
   )  
   # A1 <- data.frame(
   #   n = c(400, 400, 400, 600, 600, 600, 800, 800, 800),
@@ -22,9 +22,10 @@ make_configs <- function() {
     # s_beta = c(2, 5, 10, 15, 25),
     # sj.min = c(5, 10, 15, 20, 30),
     # sj.max = c(10, 15, 20, 30, 50)
-    s_beta = c(2, 5, 10, 25),
-    sj.min = c(5, 10, 15, 30),
-    sj.max = c(10, 15, 20, 50)
+    s_beta = c(3, 10, 20),
+    s.j = c(5, 15, 25)
+    # sj.min = c(5, 10, 15, 30),
+    # sj.max = c(10, 15, 20, 50)
   )
   
   B1 <- data.frame(
@@ -34,14 +35,20 @@ make_configs <- function() {
   )
   
   C <- data.frame(
-    b = c(1),
-    a = c(1)
+    b = c(1, .1),
+    a = c(1, .1)
   )
   
-  D <- data.frame(
-    sigma0_h = c(1),
-    sigma0_v = c(1),
+  D1 <- data.frame(
+    sigma0_h = c(1, .1)
     # cor_hv = c(.9, .7, .5, .3, .1)
+  )
+  
+  D2 <- data.frame(
+    sigma0_v = c(1, .1)
+  )
+  
+  D3 <- data.frame(
     cor_hv = c(.9, .5, .1)
   )
   
@@ -50,10 +57,11 @@ make_configs <- function() {
   )
   
   F0 <- data.frame(
-    corstr = c("c1", "c2")
+    # corstr = c("c1", "c2")
+    corstr= c("c1")
   )
   
-  configs0 <- list(A1, B0, C, D, E, F0)
+  configs0 <- list(A1, B0, C, D1, D2, D3, E, F0)
   # configs1 <- list(A1, B1, C, D, E, F0)
   configs <- list(configs0)
   configs %>%
