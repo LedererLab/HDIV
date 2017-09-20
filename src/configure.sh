9c7eeaf02b23eda0535dd492e82e7b15b107076e
David Gold <david.gold.a@gmail.com>
2017-09-20 15:12:06 -0400

    Work on configuration regime

--- src/configure.sh
+++ src/configure.sh
@@ -1,25 +1,16 @@
 #!/bin/bash
 
 # if [[ $# -lt 1 ]] ; then { echo "Wrong number of arguments"; exit 1; }; fi
-# project_dir=$(realpath $1)
-# _project=${project_dir##*src}
-# project=${_project#/}
-# res_dir=${project_dir%%/src*}/res
-# if [[ ! -d $res_dir/$project ]] ; then
-#   mkdir $res_dir/$project
-# fi
 if [[ ! -d err ]] ; then { mkdir err; }; fi
 if [[ ! -d out ]] ; then { mkdir out; }; fi
 
-config_dir=src/config
-nconfig=$(($(< $config_dir/configs.csv wc -l) - 1))
-echo $config_dir
+nconfig=$(($(< config/configs.csv wc -l) - 1))
 for config_id in $(seq 1 $nconfig) ; do
   if [[ ! -d res ]] ; then mkdir res fi
-  if [[ -f $config_dir/configure.r ]] ; then
-    if [[ ! -d $config_dir/$config_id ]] ; then
-      mkdir $config_dir/$config_id
+  if [[ -f config/configure.r ]] ; then
+    if [[ ! -d config/$config_id ]] ; then
+      mkdir config/$config_id
     fi
-    sbatch $config_dir/config.sbatch $config_dir $config_id
+    sbatch config/config.sbatch config $config_id
   fi
 done
