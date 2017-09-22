7e77d5df1386c93d196ce636deed623eef1f2c96
David Gold <david.gold.a@gmail.com>
2017-09-22 14:25:29 -0700

    Typo

--- src/hdiv.r
+++ src/hdiv.r
@@ -10,7 +10,7 @@ library(methods)
 library(lpSolve)
 library(glmnet)
 source("src/dgm.r", chdir = TRUE)
-source("src/estimator.r", chdir = TRUE)
+source("src/estimation.r", chdir = TRUE)
 source("src/utils.r", chdir = TRUE)
 source("src/trial.r", chdir = TRUE)
 
