a56aaf10c9b01d4d25dcf25535e5cb243ee3cfb7
David Gold <david.gold.a@gmail.com>
2017-09-23 12:53:40 -0700

    analyze script

--- src/analyze.r
+++ src/analyze.r
@@ -12,37 +12,40 @@ suppressMessages(source("src/utils.r"))
 # Analysis
 
 ingest <- function() {
-  df_est <- read.csv("res/est.csv")
-  df_stats <- read.csv("res/stats.csv")
+  est <- read.csv("res/est.csv")
+  stats <- read.csv("res/stats.csv")
   configs <- read.csv("configs/configs.csv")
 
-  list(est = df_est, stats = df_stats, configs = configs)
+  list(est = est, stats = stats, configs = configs)
 }
 
 cvg <-function(res) {
   est <- res$est; stats <- res$stats; configs <- res$configs
   est %>%
-    inner_join(dplyr::select(res$configs, config_id, n, sigma0_h),
+    filter(estimator == "Debiased") %>%
+    inner_join(dplyr::select(configs, config_id, n, sigma0_h),
                by = "config_id") %>%
-    inner_join(filter(res$stats, estimator == "Debiased") %>%
-                 dplyr::select(config_id, trial_id, sigma0_hhat),
+    inner_join(filter(stats, estimator == "Debiased") %>%
+                 dplyr::select(config_id, trial_id),
                by = c("config_id", "trial_id")) %>%
     mutate(cvgj = covered(estimate_j, beta0_j, SE1.la)) %>%
     group_by(config_id, j) %>%
-    summarize(sdj = sd(estimate_j),
-              cvgj = mean(cvgj),
-              SEj = mean(SE1.la)) %>%
+    summarize(cvgj = mean(cvgj),
+              ntrials = n()) %>%
+      # sdj = sd(estimate_j),
+              # SEj = mean(SE1.la)) %>%
     group_by(config_id) %>%
     summarize(cvg = mean(cvgj),
-              SE = mean(SEj),
-              sd = mean(sdj)) %>%
-    inner_join(res$configs, by = "config_id") %>%
-    inner_join(group_by(res$stats, config_id) %>%
-                 summarize(sigma0_hhat = mean(sigma0_hhat), by = "config_id"))
+              ntrials = mean(ntrials)) %>%
+              # SE = mean(SEj),
+              # sd = mean(sdj)) %>%
+    inner_join(configs, by = "config_id") %>%
+    dplyr::select(config_id, n, px, pz, s_beta, s.j, cor_hv, type, cvg, ntrials) %>%
+    arrange(type)
 }
-#
 
 ingest() %>% cvg %>% print
+
 
 # ingest() %>%
 #   summ %>%
